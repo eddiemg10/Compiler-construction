e29b48c872b39a9d466dd26d1e4ff6935adbd40b
eddiemg10 <59659920+eddiemg10@users.noreply.github.com>
2023-09-10 22:01:22 +0300

    Lab 1

---
+++ Lab1/part_2/automate.py
@@ -0,0 +1,37 @@
+import subprocess
+import os
+
+
+def compile_flex_file(input_file):
+    delete_generated_files()
+    try:
+        # Run the flex command to generate the C source code
+        subprocess.run(['flex', input_file], check=True)
+
+        # Compile the generated C code to create an executable
+        subprocess.run(['gcc', '-o', 'scan', 'lex.yy.c'], check=True)
+
+        print("Compilation successful")
+
+    except subprocess.CalledProcessError as e:
+        print(f"Compilation failed with error: {e}")
+    except FileNotFoundError:
+        print("Error: flex or gcc command not found. Please ensure they are installed.")
+
+
+def delete_generated_files():
+    try:
+        # Delete the generated lex.yy.c and scan.exe
+        os.remove('lex.yy.c')
+        os.remove('scan.exe')  # For Windows
+
+        print("Generated files deleted.")
+
+    except FileNotFoundError:
+        print("No generated files found.")
+
+
+if __name__ == "__main__":
+    # Replace 'your_input_file.l' with the name of your Flex input file
+    input_file = 'capital.l'
+    compile_flex_file(input_file)
