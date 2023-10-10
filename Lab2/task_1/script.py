3a8acd7bc52f91ce0b83711997e4cc2d60e89516
eddiemg10 <59659920+eddiemg10@users.noreply.github.com>
2023-10-10 15:44:58 +0300

    Task 1 of Lab 2

---
+++ Lab2/task_1/script.py
@@ -0,0 +1,20 @@
+"""
+The program below solves the FizzBuzz problem
+FizzBuzz is a common coding interview question
+ have been used to ...."""
+
+
+ # Loop from 1 to 100
+for i in range(1, 101):
+    # Check if the number is divisible by both 3 and 5
+    if i % 3 == 0 and i % 5 == 0:
+        print(f'FizzBuzz: {i}')  # Print FizzBuzz if number is divisible by both 3 and 5
+    # Check if the number is divisible by 3
+    elif i % 3 == 0:
+        print(f'Fizz: {i}')  # Print Fizz if number is only divisible by 3
+    # Check if the number is divisible by 5
+    elif i % 5 == 0:
+        print(f'Buzz: {i}')  # Print Buzz if number is only divisible by 5
+    else:
+        print(i)
+# End of the FizzBuzz program
